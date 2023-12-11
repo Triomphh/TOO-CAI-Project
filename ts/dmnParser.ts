76b30cfa30046755df617dd3cb26276c9c7cc9c9
Triomphh <driencourt.kevin@gmail.com>
2023-12-11 16:30:19 +0100

    Début du parser DMN

--- ts/dmnParser.ts
+++ ts/dmnParser.ts
@@ -6,8 +6,27 @@
 */
 
 import DmnModdle from 'dmn-moddle';
+import {xml2str} from './xml2str';
+import {DMN_Definitions} from './DMN-JS';
+
+
 
 export const parseDMN = async ( xmlString: string ): Promise<any> => {
 	const moddle = new DmnModdle();
 	return moddle.fromXML( xmlString );
 };
+
+
+export const handleDMNdata = async ( xml: File ) => {
+	try
+	{	
+		const xmlString = await xml2str( xml ); // Convert .xml to a string
+		const dmnData = await parseDMN( xmlString ); // Transform the xmlString into a JS object
+	
+		// ...
+	}
+	catch ( error )
+	{
+		console.error( "ERROR DURING DMN DATA HANDLING: ", error );
+	}
+}
